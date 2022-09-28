a6b3825912c0058de414b8121b20b5b9a59def3d
Irving Juárez <irvingjuarez274@gmail.com>
2022-09-27 21:48:27 -0500

    Http verbs created for tasks endpoint

--- src/routers/tasks.router.js
+++ src/routers/tasks.router.js
@@ -2,7 +2,26 @@ const { Router } = require("express");
 const tasksRouter = Router();
 
 tasksRouter.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Returning a list of tasks");
+})
+
+tasksRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Sending task: ${id}`);
+})
+
+tasksRouter.post("/", (req, res) => {
+  res.send("Creating a new task");
+})
+
+tasksRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Updating task: ${id}`);
+})
+
+tasksRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Deleting task: ${id}`);
 })
 
 module.exports = {
