657c42427317fb09644a5afcb7cb8b1efb7e7ac2
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-02 12:54:06 -0500

    TaskForm component improvement

--- client/src/components/TaskForm.tsx
+++ client/src/components/TaskForm.tsx
@@ -1,4 +1,5 @@
-import { Grid, Card, Typography, CardContent, TextField } from "@mui/material"
+import { Padding, Title } from "@mui/icons-material"
+import { Grid, Card, Typography, CardContent, TextField, Button } from "@mui/material"
 
 export const TaskForm = () => {
   return(
@@ -10,19 +11,30 @@ export const TaskForm = () => {
     >
       <Grid item xs={3}>
         <Card sx={{mt: 5}}>
-          <Typography>
+          <Typography variant="h5" sx={{padding: 1}}>
             Create Task
           </Typography>
 
           <CardContent>
-            <form>
+            <form style={{display: "flex", flexDirection: "column"}}>
               <TextField
                 variant="filled"
                 label="Title"
+                sx={{mb: 4}}
               />
+
+              <TextField 
+                variant="filled"
+                label="Description"
+                sx={{mb: 4}}
+              />
+
+              <Button variant="contained">
+                Submit
+              </Button>
             </form>
           </CardContent>
-        </Card>
+        </Card>s
       </Grid>
     </Grid>
   )
