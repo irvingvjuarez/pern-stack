a653cd5c35c0298bc5c00cee91d9d71fe6491146
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-03 20:16:30 -0500

    Button addde to edit and delete task

--- client/src/containers/Table.tsx
+++ client/src/containers/Table.tsx
@@ -1,4 +1,6 @@
-import {TableContainer, Table, TableHead, TableRow, TableCell, TableBody} from "@mui/material"
+import {TableContainer, Table, TableHead, TableRow, TableCell, TableBody, IconButton, Menu, MenuItem} from "@mui/material"
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useMemo, useState } from "react"
 import { TaskInterface } from "../types/interfaces/task.interface"
 
 interface TableViewProps {
@@ -12,6 +14,24 @@ export const TableView: React.FC<TableViewProps> = ({
 	itemProps,
 	items
 }) => {
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+	const config = useMemo(() => ({
+		options: [
+			"Update",
+			"Delete"
+		],
+		itemHeight: 48
+	}), [])
+
+	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+	const handleClose = () => {
+    setAnchorEl(null);
+  };
+
 	return (
 		<TableContainer>
 					<Table>
@@ -20,15 +40,50 @@ export const TableView: React.FC<TableViewProps> = ({
 								{headCells.map((cell) => (
 									<TableCell key={cell} align="center">{cell}</TableCell>
 								))}
+								<TableCell></TableCell>
 							</TableRow>
 						</TableHead>
 
 						<TableBody>
 							{items.map((item) => (
-								<TableRow>
+								<TableRow key={item.task_id}>
 									{itemProps.map(prop => (
 										<TableCell align="center">{item[prop]}</TableCell>
 									))}
+									<TableCell>
+										<IconButton
+											aria-label="more"
+											id="long-button"
+											aria-controls={open ? 'long-menu' : undefined}
+											aria-expanded={open ? 'true' : undefined}
+											aria-haspopup="true"
+											onClick={handleClick}
+										>
+											<MoreVertIcon />
+										</IconButton>
+
+										<Menu
+											id="long-menu"
+											MenuListProps={{
+												'aria-labelledby': 'long-button',
+											}}
+											anchorEl={anchorEl}
+											open={open}
+											onClose={handleClose}
+											PaperProps={{
+												style: {
+													maxHeight: config.itemHeight * 4.5,
+													width: '20ch',
+												},
+											}}
+										>
+											{config.options.map(option => (
+												<MenuItem key={option} onClick={handleClose}>
+													{option}
+												</MenuItem>
+											))}
+										</Menu>
+									</TableCell>
 								</TableRow>
 							))}
 						</TableBody>
