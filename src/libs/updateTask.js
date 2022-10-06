c083147b2399714d19f4a1ef208862fbf0cfbc57
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-06 15:54:19 -0500

    Changing name variables in the way hoe PUT method receives and sends the data

--- src/libs/updateTask.js
+++ src/libs/updateTask.js
@@ -1,10 +1,10 @@
 const getSingleTask = require("./getSingleTask");
 const handleError = require("./handleError");
 
-const updateTask = async (db, id, { name, description }) => {
+const updateTask = async (db, id, { title, description }) => {
   return await handleError(async () => {
     const oldTask = await getSingleTask(db, id);
-    const newName = name || oldTask.task_name;
+    const newName = title || oldTask.task_name;
     const newDescription = description || oldTask.task_description;
     const result = await db.query(
       `UPDATE tasks SET task_name = $1, task_description = $2 WHERE task_id = $3 RETURNING *`,
