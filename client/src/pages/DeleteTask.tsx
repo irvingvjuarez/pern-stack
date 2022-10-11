a0aa3459ff104e0e466c858e34ced544003f62ef
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-11 12:55:06 -0500

    Snackbar shown already in all the CRUD verbs

--- client/src/pages/DeleteTask.tsx
+++ client/src/pages/DeleteTask.tsx
@@ -1,6 +1,7 @@
 import { Container, AlertTitle, Alert, Box, Button } from "@mui/material"
 import { useLocation, useNavigate } from "react-router-dom"
 import { useAppFlow } from "../hooks/useAppFlow"
+import { useSnackbar } from "../hooks/useSnackbar";
 import { getIdFromPath } from "../services/getIdFromPath.service";
 import { requestHttp } from "../services/requestHttp.service"
 
@@ -8,6 +9,12 @@ export const DeleteTask = () => {
 	const { pathname } = useLocation();
 	let id = getIdFromPath(pathname) as string
 	const navigate = useNavigate()
+	const showSnackbar = useSnackbar()
+
+	const handleDelete = () => {
+		requestHttp({ method: "DELETE", id, navigate })
+		showSnackbar()
+	}
 
 	return(
 		<Container sx={{mt: 2}}>
@@ -18,7 +25,7 @@ export const DeleteTask = () => {
 
 			<Box sx={{mt: 5}}>
 				<Button variant="outlined" sx={{mr: 2}} onClick={useAppFlow()}>Cancel</Button>
-				<Button variant="contained" color="error" onClick={() => requestHttp({ method: "DELETE", id, navigate })} >
+				<Button variant="contained" color="error" onClick={handleDelete} >
 					Delete
 				</Button>
 			</Box>
